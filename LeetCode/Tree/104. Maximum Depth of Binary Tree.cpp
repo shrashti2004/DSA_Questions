3a974b49f49bdda7c50f98cf7b39f13372c702dc
Shrashti Bhumarkar <113536361+shrashti2004@users.noreply.github.com>
2023-11-07 01:56:57 +0530

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ LeetCode/Tree/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //for counting level 
+        int level = 0 ;
+
+        queue<TreeNode*> q;
+         
+        if(root==NULL){
+            return level ;
+        }
+        q.push(root);
+
+       while(!q.empty()){
+           int n = q.size();
+           
+           for(int i =0;i<n;i++){
+             
+             TreeNode *temp =q.front();
+              q.pop();
+             if(temp->left!=NULL){
+                 q.push(temp->left);
+
+         }
+         if(temp->right!=NULL) {
+             q.push(temp->right);
+         }
+             
+        }
+         level++;
+       }
+       return level ;
+
+    }
+};
