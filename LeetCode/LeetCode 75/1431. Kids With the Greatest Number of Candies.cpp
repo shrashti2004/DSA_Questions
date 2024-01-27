e72a6af32e285c23ca9fafd443b601c29fe7f318
Shrashti Bhumarkar <113536361+shrashti2004@users.noreply.github.com>
2024-01-28 01:30:57 +0530

    Create 1431. Kids With the Greatest Number of Candies.cpp

---
+++ LeetCode/LeetCode 75/1431. Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,42 @@
+// class Solution {
+// public:
+//     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+//         int n = candies.size();
+//         int max=INT_MIN;
+//         vector<bool>ans;
+//         for(int i=0;i<n;i++){
+//             if(max<=candies[i])
+//             { max=candies[i];
+//             }
+//         }
+//         for(int i =0;i<n;i++){
+//            candies[i]= candies[i]+extraCandies;
+//         }
+
+//         for(int i =0;i<n;i++){
+//             if(max<=candies[i])
+//               ans.push_back(true);
+//               else
+//               ans.push_back(false);
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+       int maxi=0;
+       vector<bool> ans;
+       for(int  i=0;i<candies.size();i++)
+       if(candies[i]>maxi)
+       maxi=candies[i];
+       for(int i=0;i<candies.size();i++)
+       if((candies[i]+extraCandies)>=maxi)
+       ans.push_back(true);
+       else 
+       ans.push_back(false);
+return ans;
+    }
+};
