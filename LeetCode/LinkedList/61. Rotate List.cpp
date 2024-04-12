cfa759a028834559b23ed6274dc670667cee7e0a
Shrashti Bhumarkar <113536361+shrashti2004@users.noreply.github.com>
2024-04-13 04:34:29 +0530

    Create 61. Rotate List.cpp

---
+++ LeetCode/LinkedList/61. Rotate List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || k == 0) return head;
+        ListNode* tail = head;
+        int len = 1;
+        while(tail->next != NULL) {
+            tail = tail->next;
+            len++;
+        }
+        k = k % len;
+        if(k % len == 0) return head;
+        ListNode* newTail = head;
+        for(int i=0; i<len-k-1; i++) {
+            newTail = newTail->next;
+        }
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+        tail->next = head;
+        return newHead;
+    }
+};
